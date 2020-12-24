699149dcfa3a1a7f8ef7bb9c3c0450fe55f5cd30
Philipp Busse <philipp.busse@gmx.net>
2020-12-24 15:00:13 +0100

    test: add missing tests

---
+++ src/unleash/unleash.context.spec.ts
@@ -0,0 +1,42 @@
+import { Request } from '../unleash-strategies'
+import { UnleashContext } from './unleash.context'
+
+function createRequest(
+  data: Partial<Request<{ id: string }>> = {},
+): Request<{ id: string }> {
+  return data
+}
+
+describe('UnleashContext', () => {
+  let context: UnleashContext
+
+  beforeEach(() => {
+    context = new UnleashContext(
+      createRequest({
+        ip: '1.2.3.4',
+        user: {
+          id: '123',
+        },
+        session: {
+          id: 'abc',
+        },
+      }),
+    )
+  })
+
+  test('getUserId()', () => {
+    expect(context.getUserId()).toBe('123')
+  })
+
+  test('getRemoteAddress()', () => {
+    expect(context.getRemoteAddress()).toBe('1.2.3.4')
+  })
+
+  test('getSessionId()', () => {
+    expect(context.getSessionId()).toBe('abc')
+
+    // @ts-ignore
+    context.request.session = { sessionId: 'def' }
+    expect(context.getSessionId()).toBe('def')
+  })
+})
